c09063bb21ad4aa7950d514e7293f7a0d9867077
Mark Paschal <markpasc@markpasc.org>
2012-02-20 14:34:19 -0500

    Set up some basic stuff

--- makerbase.py
+++ makerbase.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return u"Hello world!"
+@app.route('/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 
 if __name__ == '__main__':
