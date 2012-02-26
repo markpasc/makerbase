eda8e1e4241e886e3cbb5744d8a1398430c64546
Mark Paschal <markpasc@markpasc.org>
2012-02-26 15:32:28 -0500

    Make run.py also a wsgi

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
+import os
+from os.path import dirname, join
+
 from makerbase import app
 
 
+if 'MAKERBASE_SETTINGS' not in os.environ:
+    os.environ['MAKERBASE_SETTINGS'] = join(dirname(__file__), 'settings.py')
+
 app.config.from_envvar('MAKERBASE_SETTINGS')
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
