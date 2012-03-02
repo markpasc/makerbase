1229d8fd402e616592e8d45fe138060ff7267582
Mark Paschal <markpasc@markpasc.org>
2012-03-01 22:49:03 -0500

    Always find the right settings.py

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import os
-from os.path import dirname, join
+from os.path import abspath, dirname, join
 
 from makerbase import app
 
 
 if 'MAKERBASE_SETTINGS' not in os.environ:
-    os.environ['MAKERBASE_SETTINGS'] = join(dirname(__file__), 'settings.py')
+    os.environ['MAKERBASE_SETTINGS'] = join(dirname(abspath(__file__)), 'settings.py')
 
 app.config.from_envvar('MAKERBASE_SETTINGS')
 
