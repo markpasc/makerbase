d5eed71637d00694c7741bfa0aa6e37e9f5e5c4b
Mark Paschal <markpasc@markpasc.org>
2012-03-13 17:17:10 -0400

    Edit participants separately

--- makerbase/views/api.py
+++ makerbase/views/api.py
@@ -24,61 +24,44 @@ class RobjectView(MethodView):
     def render(self, obj):
         return json.dumps(obj.get_entity_data())
 
-
-class ProjectAPI(RobjectView):
-
     def get(self, slug):
-        # TODO: support a None slug for getting the project collection?
-        proj = Project.get(slug)
-        if proj is None:
+        obj = self.objclass.get(slug)
+        if obj is None:
             abort(404)
-
-        return self.render(proj)
+        return self.render(obj)
 
     @login_required
     def post(self, slug):
-        proj = Project.get(slug)
-        if proj is None:
+        obj = self.objclass.get(slug)
+        if obj is None:
             abort(404)
 
-        proj_data = json.loads(request.data)
-
-        form = ProjectForm(MultiDict(proj_data), proj)
+        data = json.loads(request.data)
+        form = self.formclass(MultiDict(data), obj)
         if not form.validate():
             return Response(json.dumps({
-                "errors": form.errors,
+                'errors': form.errors,
             }), 400)
 
-        # TODO: save a historical project item
-        form.populate_obj(proj)
-        proj.save()
+        form.populate_obj(obj)
+        obj.save()
 
-        for party_data in proj_data['parties']:
-            party_id = party_data.get('_id')
-            if party_id is None:
-                maker_id = party_data.get('maker_id')
-                if maker_id is None:
-                    continue
-                maker = Maker.get(maker_id)
-                party = Participation()
+        return self.render(obj)
 
-                party.add_link(maker, tag='maker')
-                party.add_link(project, tag='project')
-                maker.add_link(party, tag='participation')
-                proj.add_link(party, tag='participation')
-            else:
-                party = Participation.get(party_id)
 
-            form = ParticipationForm(MultiDict(party_data), party)
-            if not form.validate():
-                return Response(json.dumps({
-                    'errors': form.errors,
-                }), 400)
+class ProjectAPI(RobjectView):
 
-            form.populate_obj(party)
-            party.save()
-
-        return self.render(proj)
+    objclass = Project
+    formclass = ProjectForm
 
 
 app.add_url_rule('/api/project/<slug>', view_func=ProjectAPI.as_view('api_project'))
+
+
+class ParticipationAPI(RobjectView):
+
+    objclass = Participation
+    formclass = ParticipationForm
+
+
+app.add_url_rule('/api/participation/<slug>', view_func=ParticipationAPI.as_view('api_participation'))
